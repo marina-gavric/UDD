d5eeecdff1bca1d8d6b63f4a349b827232679356
marina-gavric <gavric.marinaa@gmail.com>
2020-02-19 10:16:34 +0100

    Izmene

---
+++ centrala/src/main/java/com/example/centrala/CentralaApplication.java
@@ -0,0 +1,13 @@
+package com.example.centrala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralaApplication.class, args);
+	}
+
+}
